793bb5eb9946516cf022218a62fc12ad728d4cf3
JayCC1 <1093033394@qq.com>
2026-02-06 19:02:40 +0800

    fix: 修复 use-merged-state 状态切换逻辑

--- src/use-merged-state.ts
+++ src/use-merged-state.ts
@@ -1,18 +1,23 @@
-import { Ref, watch, ComputedRef, computed } from 'vue'
+import { Ref, watch, ComputedRef, computed, ref } from 'vue'
 
 export default function useMergedState<T> (
   controlledStateRef: Ref<T | undefined>,
   uncontrolledStateRef: Ref<T>
 ): ComputedRef<T> {
+  // 追踪 controlledStateRef 是否曾经有过非 undefined 的值
+  const controlledRef = ref(controlledStateRef.value !== undefined)
   watch(controlledStateRef, value => {
     if (value !== undefined) {
       uncontrolledStateRef.value = value
+      controlledRef.value = true
     }
   })
   return computed(() => {
-    if (controlledStateRef.value === undefined) {
-      return uncontrolledStateRef.value
+    // 如果 controlledStateRef 曾经被设置过值，则始终使用 controlledStateRef 的值
+    // 这样当 controlledStateRef 变回 undefined 时，返回的也是 undefined
+    if (controlledRef.value) {
+      return controlledStateRef.value as T
     }
-    return controlledStateRef.value
+    return uncontrolledStateRef.value
   })
 }
